66f74c2e416667b21e4fe72443ecf85a492c8391
GaryH <henhaodegaigai@gmail.com>
2019-12-06 17:21:01 -0500

    Better Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/Auto/MainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/Auto/MainAuto.java
@@ -18,15 +18,16 @@ import FTCEngine.Math.Vector2;
 @Autonomous(name = "MainAuto")
 public class MainAuto extends AutoOpModeBase
 {
-    @Override
-    protected void awake() {
-        super.awake();
-        getInput().registerButton(Input.Source.CONTROLLER_1, Input.Button.B);
-        getInput().registerButton(Input.Source.CONTROLLER_1, Input.Button.X);
-        getInput().registerButton(Input.Source.CONTROLLER_1, Input.Button.Y);
-    }
+	@Override
+	protected void awake()
+	{
+		super.awake();
+		getInput().registerButton(Input.Source.CONTROLLER_1, Input.Button.B);
+		getInput().registerButton(Input.Source.CONTROLLER_1, Input.Button.X);
+		getInput().registerButton(Input.Source.CONTROLLER_1, Input.Button.Y);
+	}
 
-    @Override
+	@Override
 	public void addBehaviors(List<Behavior> behaviorList)
 	{
 		behaviorList.add(new Gyroscope(this));
@@ -34,33 +35,36 @@ public class MainAuto extends AutoOpModeBase
 		behaviorList.add(new Intake(this));
 		behaviorList.add(new IntakeAuto(this));
 		behaviorList.add(new LiftAuto(this));
-        behaviorList.add(new Grabber(this));
-        behaviorList.add(new GrabberAuto(this));
+		behaviorList.add(new Grabber(this));
+		behaviorList.add(new GrabberAuto(this));
 	}
 
-    private Mode mode = Mode.POSITION_1_FULL;
-    private int waitTime = 20;
+	private Mode mode = Mode.POSITION_1_FULL;
+	private int waitTime = 20;
 
-    @Override
-    protected void configLoop() {
-        super.configLoop();
+	@Override
+	protected void configLoop()
+	{
+		super.configLoop();
 
-        if (getInput().getButtonDown(Input.Source.CONTROLLER_1, Input.Button.B)) mode = mode.getNext();
+		if (getInput().getButtonDown(Input.Source.CONTROLLER_1, Input.Button.B)) mode = mode.getNext();
 
-        if (getInput().getButtonDown(Input.Source.CONTROLLER_1, Input.Button.X)) waitTime+=2;
-        if (getInput().getButtonDown(Input.Source.CONTROLLER_1, Input.Button.Y)) waitTime-=2;
+		if (getInput().getButtonDown(Input.Source.CONTROLLER_1, Input.Button.X)) waitTime += 2;
+		if (getInput().getButtonDown(Input.Source.CONTROLLER_1, Input.Button.Y)) waitTime -= 2;
 
-        waitTime = Mathf.clamp(waitTime,0,30);
+		waitTime = Mathf.clamp(waitTime, 0, 30);
 
-        telemetry.addData("Mode (B)",mode);
-        telemetry.addData("Wait time (X/Y)", waitTime);
-    }
+		telemetry.addData("Mode (B)", mode);
+		telemetry.addData("Wait time (X/Y)", waitTime);
+	}
 
-    @Override
+	@Override
 	protected void queueJobs()
 	{
 		DrivetrainAuto drivetrain = getBehavior(DrivetrainAuto.class);
 		IntakeAuto intake = getBehavior(IntakeAuto.class);
+
+		final float delay = 0.01f;
 
 //		execute(drivetrain, new DrivetrainAuto.AutoJob(DrivetrainAuto.AutoJob.Mode.MOVE, 24f));
 //		execute(drivetrain, new DrivetrainAuto.AutoJob(DrivetrainAuto.AutoJob.Mode.STRAFE, 24f));
@@ -73,69 +77,79 @@ public class MainAuto extends AutoOpModeBase
 //		wait(0.2f);
 //		execute(drivetrain, new DrivetrainAuto.AutoJob(DrivetrainAuto.AutoJob.Mode.ROTATE,-90f));
 
-        if(mode == Mode.POSITION_1_PARK||mode == Mode.POSITION_2_PARK) {
-            wait((float) waitTime);
-            execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, mode == Mode.POSITION_1_PARK?-33f:33f)));
-            return;
-        }
+		if (mode == Mode.POSITION_1_PARK || mode == Mode.POSITION_2_PARK)
+		{
+			wait((float)waitTime);
+			execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, mode == Mode.POSITION_1_PARK ? -33f : 33f)));
+			return;
+		}
 
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, 15f)));
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, -15f)));
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, 8f)));
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, -8f)));
 
-        wait(.5f);
+		wait(delay);
 
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(-39f, 0f)));
-        execute(intake, new IntakeAuto.AutoJob(1f));
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(2, 0f)));
 
-        //COLLECTION
-        wait(0.2f);
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, 10f)));
-        execute(intake ,new IntakeAuto.AutoJob(0f));
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(-42f, 0f)));
+		execute(intake, new IntakeAuto.AutoJob(1f));
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(2f, 0f)));
 
-        wait(0.2f);
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(23f, 0f)));
+		//COLLECTION
+		wait(delay);
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, 10f)));
 
-        wait(0.2f);
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, -85f)));
+		wait(delay);
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(23f, 0f)));
 
-        if(mode == Mode.POSITION_1_NO_FOUNDATION) {
+		wait(delay);
+		execute(drivetrain, new DrivetrainAuto.AutoJob(0f)); //Resets rotation
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, -82f)));
+		execute(intake, new IntakeAuto.AutoJob(0f));
 
-            execute(drivetrain, new DrivetrainAuto.AutoJob(180f));
-            wait(0.2f);
+		if (mode == Mode.POSITION_1_NO_FOUNDATION)
+		{
+			execute(drivetrain, new DrivetrainAuto.AutoJob(180f));
+			wait(delay);
 
-            execute(intake ,new IntakeAuto.AutoJob(-1f));
-            wait(2.0f);
+			execute(intake, new IntakeAuto.AutoJob(-1f));
+			wait(delay);
 
-            execute(intake ,new IntakeAuto.AutoJob(0f));
-            execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(5f, 0)));
-            wait(1.0f);
+			execute(intake, new IntakeAuto.AutoJob(0f));
+			execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(5f, 0)));
+			wait(delay);
 
-            execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, -20)));
-            return;
-        }
+			execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, -20)));
+			return;
+		}
 
-        wait(0.2f);
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(-15f, 0f)));
+		wait(delay);
+		execute(drivetrain, new DrivetrainAuto.AutoJob(0f)); //Resets rotation
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(-15f, 0f)));
 
-        //GRAB PLATFORM
-        wait(1f);
+		//GRAB PLATFORM
+		wait(1f);
 
-        wait(0.2f);
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(25f, 0f)));
+		execute(drivetrain, new DrivetrainAuto.AutoJob(0f)); //Resets rotation
 
-        //RELEASE PLATFORM
-        wait(1f);
+		wait(delay);
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(45f, 0f)));
 
-        wait(0.2f);
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(2f, 0f)));
+		//RELEASE PLATFORM
+		wait(1f);
 
-        //PARK
-        wait(0.2f);
-        execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, 45f)));
+		wait(delay);
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(2f, 0f)));
 
+		//PARK
+		wait(delay);
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, 20f)));
 
+		wait(delay);
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(-25f, 0f)));
 
-
+		wait(delay);
+		execute(drivetrain, new DrivetrainAuto.AutoJob(new Vector2(0f, 30f)));
 
 
 //		wait(1f);
@@ -155,21 +169,24 @@ public class MainAuto extends AutoOpModeBase
 //		execute(drivetrain, new DrivetrainAuto.AutoJob(DrivetrainAuto.AutoJob.Mode.STRAFE, 30f));
 	}
 
-	private enum Mode {
+	private enum Mode
+	{
 
-        POSITION_1_FULL(0),
-        POSITION_1_NO_FOUNDATION(1),
-        POSITION_1_PARK(2),
-        POSITION_2_PARK(3);
+		POSITION_1_FULL(0),
+		POSITION_1_NO_FOUNDATION(1),
+		POSITION_1_PARK(2),
+		POSITION_2_PARK(3);
 
-        Mode(int value) {
-            this.value = value;
-        }
+		Mode(int value)
+		{
+			this.value = value;
+		}
 
-        private final int value;
+		private final int value;
 
-        public Mode getNext() {
-            return Mode.values()[(value+1)%Mode.values().length];
-        }
-    }
+		public Mode getNext()
+		{
+			return Mode.values()[(value + 1) % Mode.values().length];
+		}
+	}
 }
